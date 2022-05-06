41f4c90814ed479a87026556ca753fedacc702e2
Asemaneh Nafe <asemanehnafe@gmail.com>
2022-05-07 01:48:48 +0430

    add UnitTest

--- src/main/java/views/LoginMenu.java
+++ src/main/java/views/LoginMenu.java
@@ -16,42 +16,31 @@ public class LoginMenu extends Menu
     public boolean checkCommand(String command,Scanner scanner)
     {
         Matcher matcher;
-        
-        if((matcher=Commands.getMatcher(command, Commands.LOGIN))!=null)
-        {   
+        if((matcher=Commands.getMatcher(command, Commands.LOGIN))!=null) {
             System.out.println(loginController.login(command));
             return false;
-        }else if((matcher=Commands.getMatcher(command, Commands.SHOWCURRENTMENU))!=null)
-        {
+        }else if((matcher=Commands.getMatcher(command, Commands.SHOWCURRENTMENU))!=null) {
             System.out.println("Login Menu");
             return false;
-        }else if((matcher=Commands.getMatcher(command, Commands.CREATEUSER))!=null)
-        {
+        }else if((matcher=Commands.getMatcher(command, Commands.CREATEUSER))!=null) {
             System.out.println(loginController.createUser(command));
             return false;
-        }else if((matcher=Commands.getMatcher(command, Commands.EXIT))!=null)
-        {
+        }else if((matcher=Commands.getMatcher(command, Commands.EXIT))!=null) {
             UserController.saveUsers();
             return true;
-        }else if((matcher=Commands.getMatcher(command, Commands.CHANGEMENU))!=null)
-        {
-            if(UserController.loggedInUser==null)
-            {
+        }else if((matcher=Commands.getMatcher(command, Commands.CHANGEMENU))!=null) {
+            if(UserController.loggedInUser==null) {
                 System.out.println("please login first");
                 return false;
             }
-            if(loginController.changeMenu(matcher))
-            {
+            if(loginController.changeMenu(matcher)) {
                 mainMenu.run(scanner);
                 return false;
             }
-
             //System.out.println("you can't move to "+matcher.group("menuname")+" from Login Menu");
             System.out.println("menu navigation is not possible");
             return false;
-            
         }
-
         System.out.println("invalid command!");
         return false;
     }
